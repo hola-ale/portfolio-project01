16106619c9875ecca96f3f8ccacfb528a75fffb2
Aleksandra Hola <homoncik.aleksandra@egmail.com>
2022-02-28 19:22:15 +0100

    my portfolio page

---
+++ index.js
@@ -0,0 +1,45 @@
+const navbar = document.querySelector("div.navbar");
+function debounce(method, delay) {
+    console.log("deboune")
+    clearTimeout(method._tId);
+    method._tId= setTimeout(function(){
+        method();
+    }, delay);
+}
+
+function handleNavbar(){
+    console.log("onScroll")
+    let top = window.scrollY;
+    if(top >= 60) {
+        navbar.classList.add("active-scroll")
+    } else {
+        navbar.classList.remove("active-scroll")
+    }
+}
+
+window.onscroll = function () {debounce(handleNavbar, 200);}
+
+const hamburger = document.querySelector("div.hamburger");
+const navLinks = document.querySelector("ul.navbar-links-list");
+const menuIcon = document.getElementById("hamburger");
+const closeIcon = document.getElementById("cancel-white");
+
+hamburger.addEventListener("click", function() {
+    if (navLinks.classList.contains("active-hamburger")) {
+        closeMenu()
+      } else {
+        navbar.classList.add("active-scroll")
+        navLinks.classList.add("active-hamburger");
+        closeIcon.style.display = "inline";
+        menuIcon.style.display = "none";
+      }
+})
+
+function closeMenu() {
+    navLinks.classList.remove("active-hamburger");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "inline";
+}
+navLinks.addEventListener("click", function() {
+    closeMenu()
+})
